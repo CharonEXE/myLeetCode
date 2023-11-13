cc2e212991fe550d8c72dc19f64879dfeb0cd9c7
Charon <rinrancharon.devops@gmail.com>
2023-11-14 01:00:42 +0800

    re-#283 Update the answer with better Vec method

--- exercise/283.rs
+++ exercise/283.rs
@@ -1,21 +1,7 @@
 impl Solution {
     pub fn move_zeroes(nums: &mut Vec<i32>) {
         let count = nums.len();
-        let mut left = 0;
-        let mut i = 0;
-        while i < count - 1 {
-            if count - i - 1 > left {
-                match nums[i] {
-                    0 => {
-                        nums.remove(i);
-                        nums.push(0);
-                        i -= 1;
-                        left += 1;
-                    },
-                _ => (),
-                }
-            }
-            i += 1;
-        }
+        nums.retain(|&x| x != 0);
+        nums.resize(count, 0);
     }
 }
